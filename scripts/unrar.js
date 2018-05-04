68d4a935743f92963213260aec755e375c5e2b58
Ian Horn <rihorn2@uky.edu>
2018-05-04 19:57:44 -0400

    messing unrar.js, change directories

--- scripts/unrar.js
+++ scripts/unrar.js
@@ -1,9 +1,9 @@
 var Unrar = require('node-unrar');
  
-var rar = new Unrar('data/South_America_Hydrography.rar_t=1484592815');
+var rar = new Unrar('../project-files/South_America_Hydrography.rar_t=1484592815');
  
 /// Create '/path/to/dest/' before rar.extract()
  
-rar.extract('data/SA_Hydro_unrarred.zip', null, function (err) {
+rar.extract('../data/SA_Hydro_unrarred', null, function (err) {
     //file extracted successfully.
 });
