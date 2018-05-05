9a242a8ba4659864da71e100521630300f15e715
Ian Horn <rihorn2@uky.edu>
2018-05-05 08:40:06 -0400

    unrar.js

--- scripts/unrar.js
+++ scripts/unrar.js
@@ -1,6 +1,6 @@
 var Unrar = require('node-unrar');
  
-var rar = new Unrar('../project-files/South_America_Hydrography.rar_t=1484592815');
+var rar = new Unrar('../project-files/South_America_Hydrography.rar');
  
 /// Create '/path/to/dest/' before rar.extract()
  
